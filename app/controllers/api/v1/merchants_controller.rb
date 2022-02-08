9fbd4c080ad3d30c70a3cc0f111c483f6f165e97
Tim Roeder <timothyjroeder@gmail.com>
2022-02-08 15:06:31 -0700

    App: add merchant controller `#index` logic

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
+    @merchants = Merchant.all
 
+    json_string = MerchantSerializer.new(@merchants)
+    render json: json_string
   end
 end
