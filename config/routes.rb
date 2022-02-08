83206104f38cf07f727fb34d814220aa64cf756d
Tim Roeder <timothyjroeder@gmail.com>
2022-02-08 15:59:15 -0700

    Routes: add merchant#show and item#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :merchants, only: [:index] do
+      resources :merchants, only: %i[index show] do
         resources :items, controller: :merchant_items, only: [:index]
       end
-      resources :items, only: [:index] do
+      resources :items, only: %i[index show] do
         resources :merchant, controller: :items_merchant, only: [:index]
       end
     end
