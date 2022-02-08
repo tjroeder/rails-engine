78d91468f11fdff49a610636e9622c44eead9697
Tim Roeder <timothyjroeder@gmail.com>
2022-02-08 16:00:02 -0700

    Request: add item#show with tests

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,8 +1,20 @@
 class Api::V1::ItemsController < ApplicationController
   def index
     @items = Item.all
-
     json_string = ItemSerializer.new(@items)
     render json: json_string
+  end
+
+  def show
+    @item = find_item
+    json_string = ItemSerializer.new(@item)
+    render json: json_string
+  end
+
+  private
+
+  def find_item
+    id = params.permit(:id)[:id]
+    Item.find(id)
   end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'Items API', type: :request do
   describe '#index action' do
     it 'returns a successful status' do
       create_list(:item, 3)
-
       get api_v1_items_path
 
       expect(response).to have_http_status(200)
@@ -12,9 +11,7 @@ RSpec.describe 'Items API', type: :request do
 
     it 'returns a list of all items' do
       create_list(:item, 3)
-
       get api_v1_items_path
-
       items = JSON.parse(response.body, symbolize_names: true)
 
       expect(items).to be_a(Hash)
@@ -45,7 +42,6 @@ RSpec.describe 'Items API', type: :request do
 
     it 'returns an array of data even if no items' do
       get api_v1_items_path
-
       items = JSON.parse(response.body, symbolize_names: true)
 
       expect(items).to be_a(Hash)
@@ -56,7 +52,6 @@ RSpec.describe 'Items API', type: :request do
     it 'returns an array of data if one item' do
       create(:item)
       get api_v1_items_path
-
       items = JSON.parse(response.body, symbolize_names: true)
 
       expect(items).to be_a(Hash)
@@ -67,14 +62,57 @@ RSpec.describe 'Items API', type: :request do
 
     it 'does not include dependent data' do
       create_list(:item, 3)
-
       get api_v1_items_path
-
       items = JSON.parse(response.body, symbolize_names: true)
 
       items[:data].each do |item|
         expect(item).to_not have_key(:relationships)
       end
+    end
+  end
+
+  describe '#show action' do
+    it 'returns a successful status' do
+      item = create(:item)
+      get api_v1_item_path(item)
+
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns an item' do
+      create_list(:item, 3)
+      item = create(:item)
+      get api_v1_item_path(item)
+      item_parsed = JSON.parse(response.body, symbolize_names: true)
+
+      expect(item_parsed).to be_a(Hash)
+      expect(item_parsed[:data]).to be_a(Hash)
+
+      expect(item_parsed[:data]).to have_key(:id)
+      expect(item_parsed[:data][:id]).to be_an(String)
+
+      expect(item_parsed[:data]).to have_key(:type)
+      expect(item_parsed[:data][:type]).to be_a(String)
+
+      expect(item_parsed[:data][:attributes]).to have_key(:name)
+      expect(item_parsed[:data][:attributes][:name]).to be_a(String)
+      
+      expect(item_parsed[:data][:attributes]).to have_key(:description)
+      expect(item_parsed[:data][:attributes][:description]).to be_a(String)
+
+      expect(item_parsed[:data][:attributes]).to have_key(:unit_price)
+      expect(item_parsed[:data][:attributes][:unit_price]).to be_a(Float)
+
+      expect(item_parsed[:data][:attributes]).to have_key(:merchant_id)
+      expect(item_parsed[:data][:attributes][:merchant_id]).to be_a(Integer)
+    end
+
+    it 'does not include dependent data' do
+      item = create(:item)
+      get api_v1_item_path(item)
+      item_parsed = JSON.parse(response.body, symbolize_names: true)
+
+      expect(item_parsed[:data]).to_not have_key(:relationships)
     end
   end
 end
