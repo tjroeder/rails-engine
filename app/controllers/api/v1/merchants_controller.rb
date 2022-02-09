b9a0564254dcbf2adf4c340482058a8db8770902
Tim Roeder <timothyjroeder@gmail.com>
2022-02-09 15:25:40 -0700

    Refactor: add callback action and slim down set_merchant

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,21 +1,21 @@
 class Api::V1::MerchantsController < ApplicationController
+  before_action :set_merchant, only: %i[show]
+
   def index
     @merchants = Merchant.all
 
     json_string = MerchantSerializer.new(@merchants)
-    render json: json_string
+    json_response(json_string)
   end
 
   def show
-    @merchant = find_merchant
     json_string = MerchantSerializer.new(@merchant)
-    render json: json_string
+    json_response(json_string)
   end
 
   private
 
-  def find_merchant
-    id = params.permit(:id)[:id]
-    Merchant.find(id)
+  def set_merchant
+    @merchant = Merchant.find(params[:id])
   end
 end
