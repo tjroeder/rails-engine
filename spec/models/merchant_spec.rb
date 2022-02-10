d3b23184aa84c0a0f079430853f8dcce0f73f01b
Tim Roeder <timothyjroeder@gmail.com>
2022-02-09 20:43:32 -0700

    Model: add #find_one_by_name with tests

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -18,4 +18,29 @@ RSpec.describe Merchant, type: :model do
       expect(merchant).to have_attributes(name: 'Wacky Wally World')
     end
   end
+
+  describe 'class methods' do
+    describe '::find_one_by_name' do
+      it 'should return a merchant from case insensitive search' do
+        create(:merchant, name: 'Turing')
+        create(:merchant, name: 'Ring World')
+        create(:merchant, name: 'Abc')
+        expected = create(:merchant, name: 'Boringo')
+  
+        expect(Merchant.find_one_by_name('Ring')).to eq(expected)
+        expect(Merchant.find_one_by_name('ring')).to eq(expected)
+        expect(Merchant.find_one_by_name('RING')).to eq(expected)
+        expect(Merchant.find_one_by_name('rInG')).to eq(expected)
+      end
+
+      it 'should return a merchant case sensitive alphabetically if multiple matches' do
+        create(:merchant, name: 'Turing')
+        create(:merchant, name: 'aRing World')
+        create(:merchant, name: 'Abc')
+        expected = create(:merchant, name: 'Boringo')
+  
+        expect(Merchant.find_one_by_name('ring')).to eq(expected)
+      end
+    end
+  end
 end
