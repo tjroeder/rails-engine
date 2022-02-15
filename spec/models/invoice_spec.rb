bbbd27cf1e677cd7ae641bf91f4b11e0754a06e3
Tim Roeder <timothyjroeder@gmail.com>
2022-02-15 15:58:38 -0700

    Model: update for transaction and customer

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
   describe 'associations' do
+    it { should belong_to(:merchant) }
+    it { should belong_to(:customer) }
     it { should have_many :invoice_items }
     it { should have_many(:items).through(:invoice_items) }
+    it { should have_many :transactions }
   end
 
   describe 'validations' do
