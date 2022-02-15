648a9c8a45349b2595760dc4007540e04e07e571
Tim Roeder <timothyjroeder@gmail.com>
2022-02-15 15:53:39 -0700

    Request: add merchants_controller#most_items

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -13,6 +13,17 @@ class Api::V1::MerchantsController < ApplicationController
     json_response(json_string)
   end
 
+  def most_items
+    number = params[:quantity]
+    if number.present?
+      @merchants = Merchant.most_items_sold(number)
+      json_string = ItemsSoldSerializer.new(@merchants)
+      json_response(json_string)
+    else
+      json_response({ error: 'Quantity is needed' }, :bad_request)
+    end
+  end
+
   private
 
   def set_merchant
