7e0d2b80c1e28a8281bccc590f9da94c15009b13
Tim Roeder <timothyjroeder@gmail.com>
2022-02-15 15:56:38 -0700

    Model: update transaction association

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items, dependent: :destroy
+  has_many :transactions, through: :invoices
 
   validates :name, :description, :unit_price, presence: true
   validates :unit_price, numericality: { greater_than: 0 }
