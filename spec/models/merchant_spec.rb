91d58093da0c7b919c4566286067f87e399d7ae9
Tim Roeder <timothyjroeder@gmail.com>
2022-02-15 16:01:09 -0700

    Model: update for new associations

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe Merchant, type: :model do
   describe 'associations' do
     it { should have_many :items }
+    it { should have_many :invoices }
+    it { should have_many(:invoice_items).through(:items) }
+    it { should have_many(:transactions).through(:invoices) }
+    it { should have_many(:customers).through(:invoices) }
   end
 
   describe 'validations' do
